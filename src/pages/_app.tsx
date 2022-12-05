ff70d6a6b0db7265bfec6fcce288a0374af8de23
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-05 19:34:15 +0900

    Add _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "styles/theme";
+import { ApolloProvider } from "@apollo/client";
+import client from "utils/client";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ChakraProvider>
   );
 }
