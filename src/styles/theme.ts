48df7b1275fe872b7608c80fc4b43de773e8d9db
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-05 19:32:35 +0900

    Add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        background: "#645681",
+      },
+    },
+  },
+});
+
+export default theme;
