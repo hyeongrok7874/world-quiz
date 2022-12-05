d54f2cba1c085373d83b7fdfdf5b57704f671d88
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-05 19:34:02 +0900

    Add Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { NextPage } from "next";
+import Link from "next/link";
+import * as I from "Assets/svg";
 
-export default function Home() {
-  return <Box>안녕하세요</Box>;
-}
+const Home: NextPage = () => {
+  return (
+    <Flex justifyContent="center">
+      <Flex
+        w="600px"
+        h="100vh"
+        bg="#FDDCD6"
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <I.Logo />
+        <Link href="/quiz/flag" style={{ marginTop: "100px" }}>
+          <I.StartIcon />
+        </Link>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Home;
