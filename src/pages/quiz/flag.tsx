41ceb4a9e25cc6ceee800959e9fc0c663aef2d6d
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-10 20:55:32 +0900

    Update flag

--- src/pages/quiz/flag.tsx
+++ src/pages/quiz/flag.tsx
@@ -1,39 +1,64 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
 import { Text, Flex, Image, Heading } from "@chakra-ui/react";
 import { AnswerButton } from "components";
 import { GetStaticProps, NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import client from "utils/client";
 
 interface dataType {
   data: {
     countries: {
       code: string;
+      name: string;
     }[];
   };
 }
 
 interface FlagType {
-  codes: string[];
+  countries: CountryType[];
 }
 
-const GET_COUNTRY = gql`
-  query GetCountry($code: ID!) {
-    country(code: $code) {
-      name
-      code
-    }
-  }
-`;
+interface CountryType {
+  code: string;
+  name: string;
+}
 
-const Flag: NextPage<FlagType> = ({ codes }) => {
-  const [randomIndex, setRandomIndex] = useState<number>(
-    Math.floor(Math.random() * codes.length)
-  );
-  const [code, setCode] = useState<String>(codes[randomIndex]);
-  const { data, loading } = useQuery(GET_COUNTRY, {
-    variables: { code: code },
-  });
+const Flag: NextPage<FlagType> = ({ countries }) => {
+  const [answer, setAnswer] = useState<CountryType>();
+  const [example, setExample] = useState<CountryType[]>([]);
+  const [correctCount, setCorrectCount] = useState<number>(0);
+  const indexArray: number[] = [];
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    setAnswer(countries[randomIndex]);
+    setExample([countries[randomIndex]]);
+  }, []);
+
+  useEffect(() => {
+    getRandomIndex();
+  }, [answer]);
+
+  const getRandomIndex = () => {
+    if (answer) {
+      while (indexArray.length < 3) {
+        const exampleIndex = Math.floor(Math.random() * countries.length);
+        const exampleCode = countries[exampleIndex].code;
+        if (
+          answer?.code !== exampleCode &&
+          !example.map((exam) => exam.code).includes(exampleCode)
+        ) {
+          indexArray.push(exampleIndex);
+        }
+      }
+      return setExample([
+        ...example,
+        ...indexArray.map((index) => countries[index]),
+      ]);
+    } else {
+      return;
+    }
+  };
+
   return (
     <Flex justifyContent="center">
       <Flex
@@ -43,25 +68,43 @@ const Flag: NextPage<FlagType> = ({ codes }) => {
         alignItems="center"
         justifyContent="center"
         flexDir="column"
+        position="relative"
       >
-        {loading ? (
-          <Text fontWeight="700" fontSize="40px">
+        <Text
+          position="absolute"
+          left="10px"
+          top="10px"
+          color="#8675A9"
+          fontSize="30px"
+          fontWeight="700"
+        >
+          {correctCount} 점
+        </Text>
+        <Heading color="#8675A9" mb="30px">
+          무엇이 정답일까요?
+        </Heading>
+        {answer ? (
+          <Image
+            width="400px"
+            height="300px"
+            alt="flag"
+            src={`https://flagcdn.com/${answer?.code.toLowerCase()}.svg`}
+          />
+        ) : (
+          <Text fontSize="40px" fontWeight="700" color="#C3AED6">
             잠시만요...
           </Text>
-        ) : (
-          <>
-            <Heading color="#8675A9" mb="30px">
-              무엇이 정답일까요?
-            </Heading>
-            <Image
-              width="400px"
-              height="300px"
-              alt="flag"
-              src={`https://flagcdn.com/${data?.country.code.toLowerCase()}.svg`}
-            />
-            <AnswerButton name={data?.country.name}></AnswerButton>
-          </>
         )}
+        {example
+          .sort(() => Math.random() - 0.5)
+          .map((country, index) => (
+            <AnswerButton
+              key={index}
+              correct={country.code === answer?.code}
+              name={country.name}
+              setCorrectCount={setCorrectCount}
+            />
+          ))}
       </Flex>
     </Flex>
   );
@@ -71,6 +114,7 @@ const GET_COUNTRIES = gql`
   query Countries {
     countries {
       code
+      name
     }
   }
 `;
@@ -85,7 +129,10 @@ export const getStaticProps: GetStaticProps = async () => {
 
     return {
       props: {
-        codes: countries.map((country) => country.code),
+        countries: countries.map((country) => ({
+          code: country.code,
+          name: country.name,
+        })),
       },
     };
   } catch (e) {
