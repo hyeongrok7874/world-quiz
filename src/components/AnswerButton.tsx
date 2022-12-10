af5cf641c7ae93f9d1dc092eefd12b41277ecf9f
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-10 20:44:54 +0900

    Add AnswerButton

--- src/components/AnswerButton.tsx
+++ src/components/AnswerButton.tsx
@@ -1,11 +1,38 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { Dispatch, SetStateAction } from "react";
 
 interface PropsType {
   name: string;
+  correct: boolean;
+  setCorrectCount: Dispatch<SetStateAction<number>>;
 }
 
-const AnswerButton: NextPage<PropsType> = ({ name }) => {
+const AnswerButton: NextPage<PropsType> = ({
+  name,
+  correct,
+  setCorrectCount,
+}) => {
+  const toast = useToast();
+  const clinkFunc = () => {
+    correct
+      ? (toast({
+          position: "top-right",
+          title: "정답",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        }),
+        setCorrectCount((prev) => prev + 1))
+      : (toast({
+          position: "top-right",
+          title: "오답",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        }),
+        setCorrectCount((prev) => (prev < 1 ? prev : prev - 1)));
+  };
   return (
     <Button
       w="400px"
@@ -14,6 +41,7 @@ const AnswerButton: NextPage<PropsType> = ({ name }) => {
       colorScheme="purple"
       fontSize="30px"
       m="20px 0"
+      onClick={clinkFunc}
     >
       {name}
     </Button>
