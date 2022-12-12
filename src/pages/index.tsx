304386bae0c97d840ca1f8078778eb618524bad0
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-12 19:36:36 +0900

    Add responsive design home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,12 +23,27 @@ const Home: NextPage = () => {
           flexDir="column"
           alignItems="center"
           justifyContent="center"
+          css={{
+            "@media (max-width: 600px": {
+              width: "100%",
+            },
+          }}
         >
           <I.Logo />
-          <Text fontSize="25px" fontWeight="500" mt="50px" color="#8675A9">
+          <Text
+            fontSize="25px"
+            fontWeight="500"
+            mt="50px"
+            color="#8675A9"
+            css={{
+              "@media (max-width: 350px)": {
+                fontSize: "20px",
+              },
+            }}
+          >
             My Best Score : {bestScore}
           </Text>
-          <Link href="/quiz/flag" style={{ marginTop: "100px" }}>
+          <Link href="/quiz/flag" style={{ marginTop: "20%", width: "50%" }}>
             <I.StartIcon />
           </Link>
         </Flex>
