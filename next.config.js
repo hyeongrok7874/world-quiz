b0b6f683f40f0ba069218d4dba1aca2b27db082a
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-13 17:21:40 +0900

    Add env

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const withPWA = require("next-pwa")({
   dest: "public",
 });
 
-module.exports = withPWA({});
+module.exports = withPWA({
+  env: {
+    NAVER_API_ID: process.env.NAVER_API_ID,
+    NAVER_API_SECRET: process.env.NAVER_API_SECRET,
+  },
+});
