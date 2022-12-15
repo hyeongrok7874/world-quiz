aa3de849901e8b8524ca114ca92fbce2d52a15df
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-15 23:09:54 +0900

    Add Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "styles/theme";
 import { ApolloProvider } from "@apollo/client";
 import client from "utils/client";
+import Fonts from "styles/Fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <ApolloProvider client={client}>
         <Component {...pageProps} />
       </ApolloProvider>
