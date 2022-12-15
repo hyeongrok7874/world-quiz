6f2d300be01789ccdb698213486cc8c9f79f604c
hyeongrok7874 <khr5k0228@gmail.com>
2022-12-15 23:09:14 +0900

    Add Fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
+  fonts: {
+    body: `'twayair', sans-serif`,
+  },
   styles: {
     global: {
       "html, body": {
